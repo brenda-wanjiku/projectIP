255b7b5bb96278acc1fcc4ef0fe9028f6a644f24
brenda-wanjiku <brendawanjiku@live.com>
2020-06-06 10:05:05 +0300

    Creates Profile and Project models

--- project/models.py
+++ project/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    '''
+    Class that defines Project attributes
+    '''
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='project/')
+    description = models.CharField(max_length=200)
+    link = models.CharField(max_length=100)
+    pub_date = models.DateField(auto_now_add=True)
+
+
+
+class Profile(models.Model):
+    '''
+    Class that defines Profile attributes
+    '''
+    user = models.CharField(max_length=50)
+    bio  =  models.CharField(max_length=100) 
+    contact =  models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='project/')
+
+
+
+
+
+
--- projectIP/settings.py
+++ projectIP/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -144,3 +145,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR,"static")
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
